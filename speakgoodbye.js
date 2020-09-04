bc1f941e8a380079f56bada3093b2d2df891e71e
Ishita <ishitasrivastavalko@gmail.com>
2020-09-04 18:51:10 +0530

    Create speakgoodbye.js

---
+++ speakgoodbye.js
@@ -0,0 +1,11 @@
+
+/*this file is to be saved as speakgoodbye.js*/
+
+(function(window) {
+    var byeSpeaker = new Object();
+    var speakWord = "Good Bye";
+    byeSpeaker.speak = function speak(name) {
+        console.log(speakWord + " " + name);
+    };
+    window.byeSpeaker = byeSpeaker;
+})(window);
